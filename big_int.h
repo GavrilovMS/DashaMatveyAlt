02c5e868ebbb2aeb25003c4278e7d974d3ed79f2
GavrilovMS <94075839+GavrilovMS@users.noreply.github.com>
2022-05-11 14:59:42 +0300

    Update big_int.h

--- big_int.h
+++ big_int.h
@@ -1,16 +1,21 @@
 #include <vector>
 #include <iostream>
 #include <string>
+
 class Big_int {
 private:
 	std::vector <unsigned short> num;
-	
-	
 public:
 	
 	Big_int(std::vector<unsigned short> number = { 0 });
 	
 	Big_int& operator = (std::string str);
+	bool operator==(Big_int right);
+	bool operator!=(Big_int right);
+	bool operator<(Big_int right);
+	bool operator>(Big_int right);
+	bool operator<=(Big_int right);
+	bool operator>=(Big_int right);
 	unsigned short& operator[](size_t index);
 	void push_back(unsigned short a);
 	void pop_back();
@@ -24,8 +29,11 @@ char less_for_big_int(Big_int a, Big_int b, bool del);
 Big_int summa(Big_int a, Big_int b);
 Big_int difference(Big_int a, Big_int b);
 Big_int multiplication(Big_int a, Big_int b);
+Big_int pow(Big_int a, Big_int pow);
 Big_int reduce_big_int(Big_int minuend, Big_int subtrahend);
 Big_int inc_big_int(Big_int a);
 Big_int div_big_int(Big_int a, Big_int b);
 Big_int mod(Big_int a, Big_int b);
-Big_int power_mod(Big_int a, Big_int b);
+Big_int power_mod(Big_int a, Big_int power, Big_int MOD);
+Big_int to_big(long long a);
+Big_int random(Big_int a);
