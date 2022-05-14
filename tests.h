2893cd046e74fa5e8d9f3f16ff597a4292522de5
GavrilovMS <94075839+GavrilovMS@users.noreply.github.com>
2022-05-14 18:19:30 +0300

    Update tests.h

--- tests.h
+++ tests.h
@@ -8,5 +8,4 @@ big_integer NOD(big_integer a, big_integer b);
 char test_Luka(big_integer n, const std::vector <big_integer>& fact_m);
 char test_Luka(big_integer n);
 void T_Diemitko(big_integer q, big_integer left_edge, big_integer right_edge);
-void T_Poklingtona();
 void T_Luka_Lemera(big_integer n);
