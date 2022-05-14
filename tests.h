c2f432260729f74a3569146e9fcf68863c6d6474
GavrilovMS <94075839+GavrilovMS@users.noreply.github.com>
2022-05-14 13:08:25 +0300

    Update tests.h

--- tests.h
+++ tests.h
@@ -5,7 +5,6 @@
 std::vector <big_integer> fact(big_integer m);
 big_integer NOD(big_integer a, big_integer b);
 
-char test_Luka(big_integer n);
-char test_Poklingtona(big_integer n);
-void T_Prota();
+char test_Luka(big_integer n, const std::vector <big_integer>& fact_m);
 void T_Diemitko(big_integer q);
+void T_Poklingtona();
